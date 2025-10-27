e91133b2c356fcb06988799c4c1f2577690a9c53
hujt <89085657@qq.com>
2025-10-27 15:48:29 +0800

    优化切换为通达信的数据读取

--- data_api.py
+++ data_api.py
@@ -1,13 +1,85 @@
 """
 使用akshar获取股票数据的API接口
 """
+import os
 import datetime
 import pandas as pd
 import numpy as np
-import utils_akshare as utils
-from show_result import create_styled_table
+import akshare as ak
 from typing import List
+from tqdm import tqdm
+from typing import List
+from mootdx.reader import Reader
+from show_result import create_styled_table
 from requests.exceptions import ConnectionError
+
+data_path = r"E:\py-workspace\stock\data"
+tdx_path = r'D:\new_tdx'
+class CustomException(Exception):
+    """自定义错误"""
+
+def trade_date():
+    """获取交易日期"""
+    path = os.path.join(data_path, "trade_date.csv")
+    if os.path.exists(path):
+        df = pd.read_csv(path)
+        df["trade_date"] = pd.to_datetime(df["trade_date"]).dt.date
+    else:
+        df = ak.tool_trade_date_hist_sina()
+        df.to_csv(path, index=False)
+    return df
+
+def cache(name):
+    """单个文件读取的装饰器"""
+    def decorator(func):
+        def wrapper(date_str, *args, **kwargs):
+            folder = os.path.join(data_path, date_str)
+            if not os.path.exists(folder):
+                os.makedirs(folder)
+            path = os.path.join(folder, f"{name}.csv")
+            if os.path.exists(path):
+                df = pd.read_csv(path)
+            else:
+                df: pd.DataFrame = func(date_str, *args, **kwargs)
+                df.to_csv(path, index=False)
+            return df
+        return wrapper
+    return decorator
+
+@cache("szse_summary")
+def szse_summary(date_str):
+    """获取深证交易所的成交数据
+    
+    Args:
+        date_str (str): 日期,例如20251011
+    """
+    rd = ak.stock_szse_summary(date=date_str)
+    if len(rd) < 14:
+        raise CustomException("深圳交易所的每日概况数据还没有更新")
+    return rd
+
+@cache("shse_summary")
+def shse_summary(date_str):
+    """获取上证交易所的成交数据"""
+    return ak.stock_sse_deal_daily(date=date_str)
+
+def shsz_amount(date_range: List[datetime.time]) -> List[pd.DataFrame]:
+    """获取沪深两市的成交额数据"""
+    reader = Reader.factory(market="std", tdxdir=tdx_path)
+    rd = []
+    file2pattern = {
+        "sh": ("sh60", "sh688"),
+        "sz": ("sz000", "sz300")
+    }    
+    for key, value in file2pattern.items():
+        spot_list = os.listdir(os.path.join(tdx_path, "vipdoc", key, "lday"))
+        spot_list = [name[2:-4] for name in spot_list if name.startswith(value)]
+        for symbol in tqdm(spot_list):
+            df = reader.daily(symbol=symbol)
+            series = df["amount"].reindex(date_range)
+            rd.append(pd.DataFrame([series.values], columns=date_range, index=[symbol]))
+    rd = pd.concat(rd)
+    return rd
 
 def get_trade_date() -> List[datetime.time]:
     """获取交易日历"""
@@ -15,8 +87,8 @@ def get_trade_date() -> List[datetime.time]:
     if today.hour >= 15:
         today = today + datetime.timedelta(days=1)
     today = today.date()
-    start_date = datetime.datetime.strptime('20251013', '%Y%m%d').date()
-    df_trade_date = utils.trade_date()
+    start_date = datetime.datetime.strptime('20251015', '%Y%m%d').date()
+    df_trade_date = trade_date()
     df_trade_date = df_trade_date[df_trade_date['trade_date'] < today]
     df_trade_date = df_trade_date[df_trade_date['trade_date'] >= start_date]
     df_trade_date = df_trade_date.tail(28)
@@ -24,44 +96,51 @@ def get_trade_date() -> List[datetime.time]:
     return df_trade_date
 
 def get_szse_summary(date_range: List[datetime.time]) -> List[float]:
-    """获取深圳证券交易所的成交数据:(亿元)"""
+    """获取深圳证券交易所的成交数据,单位(亿元)"""
     rd = []
     for search_date in date_range:
         search_date = search_date.strftime('%Y%m%d')
-        df = utils.szse_summary(search_date)
+        df = szse_summary(search_date)
         df = df.loc[df["证券类别"].isin(["主板A股", "创业板A股"])]
         rd.append(df["成交金额"].sum() / 100000000)
     return rd
 
 def get_shse_summary(date_range: List[datetime.time]) -> List[float]:
-    """获取上证证券交易所的成交数据(亿元)"""
+    """获取上证证券交易所的成交数据,单位(亿元)"""
     rd = []
     for search_date in date_range:
         search_date = search_date.strftime('%Y%m%d')
-        df = utils.shse_summary(search_date)
+        df = shse_summary(search_date)
         df = df.loc[df["单日情况"] == "成交金额", ["主板A", "科创板"]]
         rd.append(df.iloc[0].sum())
     return rd
 
-def get_dfcf_concept_summary(date_range: List[datetime.time], name) -> List[float]:
-    """获取东方财富概念板块的历史成交额数据"""
-    df: pd.DataFrame = utils.concept_summary(date_range, name)
-    df["成交额"] = df["成交额"] / 100000000
-    return df["成交额"].tolist()
-
 def get_top20_summary(date_range: List[datetime.time]) -> List[float]:
-    """获取沪深每日成交额前20的总计额度"""
+    """获取沪深每日成交额前20的总计额度,单位(亿元)"""
+    df: pd.DataFrame = shsz_amount(date_range)
     rd = []
-    for search_date in date_range:
-        search_date = search_date.strftime('%Y%m%d')
-        data1 = utils.sh_a_spot(search_date)
-        data2 = utils.sz_a_spot(search_date)
-        data3 = utils.cy_a_spot(search_date)
-        data4 = utils.kc_a_spot(search_date)
-        df = pd.concat([data1, data2, data3, data4], axis=0)
-        top20_amount = df["成交额"].nlargest(20).sum()
+    for col in df.columns:
+        top20_amount = df[col].nlargest(20).sum()
         rd.append(top20_amount / 100000000)
-    return rd    
+    return rd
+
+def get_index_summary(date_range: List[datetime.time], symbol) -> List[float]:
+    """获取指数概念的历史成交额数据"""
+    reader = Reader.factory(market='ext', tdxdir=tdx_path)
+    df = reader.daily(symbol=symbol)
+    series = df["hk_stock_amount"].reindex(date_range)
+    series = series / 100
+    return series.tolist()
+
+
+def get_concept_summary(date_range: List[datetime.time], symbol) -> List[float]:
+    """获取概念板块的历史成交额数据,单位(亿元)"""
+    reader = Reader.factory(market='std', tdxdir=tdx_path)
+    df = reader.daily(symbol=symbol)
+    series = df["amount"].reindex(date_range)
+    series = series / 100000000
+    return series.tolist()
+    
 
 def data2html(dates, total, hs300, micro, hundred, top20):
     df_source = pd.DataFrame({
@@ -131,11 +210,11 @@ def main():
     sh_amount = get_shse_summary(trade_date)
     total_amout = (np.array(sz_amount) + np.array(sh_amount)).tolist()
     print("获取沪深300成交数据")
-    hs300_amount = get_dfcf_concept_summary(trade_date, "HS300_")
+    hs300_amount = get_index_summary(trade_date, '62#000300')
     print("获取微盘股成交数据")
-    micro_amount = get_dfcf_concept_summary(trade_date, "微盘股")
+    micro_amount = get_concept_summary(trade_date, "880823")
     print("获取百元股成交数据")
-    hundred_amount = get_dfcf_concept_summary(trade_date, "百元股")
+    hundred_amount = get_concept_summary(trade_date, "880878")
     print("将数据转化成所需图表")
     data2html(trade_date, total_amout, hs300_amount, micro_amount, hundred_amount, top20_amount)
 
